662afdeca5e455c3b2f657b30a4f992dcd820a07
hyeon <lhyeonnit@gmail.com>
2024-01-30 14:39:07 +0900

    회원가입

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,11 +1,63 @@
 package shop.mtcoding.blog.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
+
+/**
+ * 컨트롤러
+ * 1. 요청받기 (URL - URI 포함)
+ * 2. http body는 어떻게? (DTO)
+ * 3. 기본 MIME 전략 : x-www-form-urlencoded (username=ssar&password=1234)
+ * 4. 유효성 검사하기 (body 데이터가 있다면)
+ * 5. 클라이언트가 view만 원하는지 혹은 DB 처리 후 view(여기서는 디스패처)도 원하는지
+ * 6. view만 원하면 view를 응답하면 끝
+ * 7. DB처리를 원하면 Model(DAO)에게 위임 후 view를 응답하면 끝
+ */
+@RequiredArgsConstructor
 @Controller
 public class UserController {
+    private UserRepository userRepository;
+    private HttpSession session;
+//    public UserController(){
+//        System.out.println("기본 생성자");
+//    }
+//    public UserController(UserRepository userRepository){
+//        System.out.println("풀 생성자");
+//        this.userRepository = userRepository;
+//    }
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO requestDTO){
+        // 1. 유효성 검사
+        if (requestDTO.getUsername().length()<3){
+            return "error/400";
+        }
+        // 2. 모델 필요
+        User user = userRepository.findByUsernameAndPassword(requestDTO);
+        System.out.println(user);
+        if (user==null){
+            return "error/401";
+        }else {
+            session.setAttribute("sessionUser",user);
+        }
+        // 3. 응답
+        return "redirect:/";
+    }
+    @PostMapping("/join")
+    public String join(UserRequest.JoinDTO requestDTO){
+        System.out.println(requestDTO);
 
+        // 1. 유효성 검사
+        if (requestDTO.getUsername().length()<3){
+            return "error/400";
+        }
+        // 2. modle에게 위임하기
+        userRepository.save(requestDTO);
+        return "redirect:/loginForm";
+    }
     @GetMapping("/joinForm")
     public String joinForm() {
         return "user/joinForm";
