7e69bfee9966266ad7bdb3aa4b3b8a016e4da137
hyeon <lhyeonnit@gmail.com>
2024-01-31 09:27:33 +0900

    login 오류 잡기

--- src/main/java/shop/mtcoding/blog/user/UserRepository.java
+++ src/main/java/shop/mtcoding/blog/user/UserRepository.java
@@ -37,7 +37,11 @@ public class UserRepository {
         Query query = em.createNativeQuery("select * from user_tb where username=? and password=?", User.class);
         query.setParameter(1, requestDTO.getUsername());
         query.setParameter(2, requestDTO.getPassword());
-        User user = (User) query.getSingleResult();
-        return user;
+        try{
+            User user = (User) query.getSingleResult();
+            return user;
+        }catch (Exception e){
+            return null;
+        }
     }
 }
