fa377af269002cbcb3518f78beacbee9cf87c2e1
hyeon <lhyeonnit@gmail.com>
2024-02-01 12:48:44 +0900

    @RequestParam

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -3,6 +3,7 @@ package shop.mtcoding.blog.board;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import shop.mtcoding.blog.user.User;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +16,8 @@ public class BoardController {
     private final HttpSession session;
     private final BoardRepository boardRepository;
     @GetMapping({ "/", "/board" })
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request, @RequestParam(defaultValue = "0") int page) {
+        System.out.println("페이지: "+page);
         List<Board> boardList = boardRepository.findAll();
         System.out.println(boardList);
 //        User sessionUser = (User) session.getAttribute("sessionUser");
