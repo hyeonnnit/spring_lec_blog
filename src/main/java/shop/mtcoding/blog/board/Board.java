55a3b3d707557869f93b99700cfa4f1773fa4d66
hyeon <lhyeonnit@gmail.com>
2024-02-02 17:12:41 +0900

    맛보기

--- src/main/java/shop/mtcoding/blog/board/Board.java
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -1,23 +1,25 @@
-package shop.mtcoding.blog.user;
+package shop.mtcoding.blog.board;
+
 
 import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
+import shop.mtcoding.blog.user.User;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name = "user_tb")
-public class User {
+@Table(name = "board_tb")
+public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(unique = true)
-    private String username;
-    @Column(length = 60,nullable = false)
-    private String password;
-    private String email;
+    private String title;
+    private String content;
+    @ManyToOne
+    private User user;
+
     @CreationTimestamp
     private LocalDateTime createdAt;
 }
--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -4,20 +4,23 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import shop.mtcoding.blog.user.User;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
     private final HttpSession session;
+    private final BoardRepository boardRepository;
+    @GetMapping("/api/board/{id}")
+    public @ResponseBody Board apiBoard(@PathVariable int id){
+        return boardRepository.findById(id);
+    }
     @GetMapping({ "/", "/board" })
     public String index() {
         User sessionUser = (User) session.getAttribute("sessionUser");
-//        if (sessionUser == null){
-//            System.out.println("로그인 안된 상태입니다.");
-//        }else {
-//            System.out.println("로그인 된 상태입니다.");
-//        }
+
         return "index";
     }
 
