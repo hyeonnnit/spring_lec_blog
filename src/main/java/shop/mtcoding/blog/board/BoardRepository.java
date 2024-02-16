b6206766b3f7a65f2c0c8c80a2a9fa99f5530a48
hyeon <lhyeonnit@gmail.com>
2024-02-16 15:53:37 +0900

    최종

--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -13,6 +13,12 @@ import java.util.List;
 public class BoardRepository {
     private final EntityManager em;
 
+    public List<Board> findAll(Integer page, String keyword) {
+        Query query = em.createNativeQuery("select * from board_tb where title like ? order by id desc limit ?, 3", Board.class);
+        query.setParameter(1, "%"+keyword+"%");
+        query.setParameter(2, page*3);
+        return query.getResultList();
+    }
     public List<Board> findAll(Integer page) {
         Query query = em.createNativeQuery("select * from board_tb order by id desc limit ?, 3", Board.class);
         query.setParameter(1, page*3);
@@ -21,6 +27,11 @@ public class BoardRepository {
 
     public Long count() {
         Query query = em.createNativeQuery("select count(*) from board_tb");
+        return (Long)query.getSingleResult();
+    }
+    public Long count(String keyword) {
+        Query query = em.createNativeQuery("select count(*) from board_tb where title like ?");
+        query.setParameter(1,"%"+keyword+"%");
         return (Long)query.getSingleResult();
     }
 
